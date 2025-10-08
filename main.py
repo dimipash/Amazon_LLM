b3fca49c7a60b76c7866d3f8eee25d578bf245e8
Dimitar Pashev <dim.pashev@gmail.com>
2025-10-08 15:08:08 +0300

    displaying all products

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import streamlit as st
-from src.oxylabs_client import scrape_product_details
-from src.services import  scrape_and_store_product
+from src.services import scrape_and_store_product, fetch_and_store_competitors
+from src.db import Database
+from src.llm import analyze_competitors
+
 
 def render_header():
     st.title("Amazon Competitor Analysis")
-    st.caption("Enter your ASIN to get product insights")
+    st.caption("Enter your ASIN to get product insights.")
+
 
 def render_inputs():
-    asin = st.text_input("ASIN", placeholder="e.g., BOXVBA224466")
-    geo = st.text_input("Zip/Postal Code", placeholder="e.g., 8000")
+    asin = st.text_input("ASIN", placeholder="e.g., B0CX23VSAS")
+    geo = st.text_input("Zip/Postal Code", placeholder="e.g., 83980")
     domain = st.selectbox("Domain", [
-        "com", "co.uk", "de", "fr", "it", "es", "ae"
+        "com", "ca", "co.uk", "de", "fr", "it", "ae"
     ])
     return asin.strip(), geo.strip(), domain
+
 
 def render_product_card(product):
     with st.container(border=True):
@@ -45,16 +49,64 @@ def render_product_card(product):
                 st.session_state["analyzing_asin"] = product["asin"]
 
 def main():
-    st.set_page_config(page_title="Amazon Competitor Analysis", page_icon=":robot:", layout="wide")
+    st.set_page_config(page_title="Amazon Competitor Analysis", page_icon="📚", layout="wide")
     render_header()
     asin, geo, domain = render_inputs()
 
-    if st.button("Scrape product") and asin:
+    if st.button("Scrape Product") and asin:
         with st.spinner("Scraping product..."):
-            st.write("Scrape")
-            product = scrape_and_store_product(asin, geo, domain)
+            scrape_and_store_product(asin, geo, domain)
         st.success("Product scraped successfully!")
-        render_product_card(product)
+
+    db = Database()
+    products = db.get_all_products()
+    if products:
+        st.divider()
+        st.subheader("Product Scraped")
+
+        items_per_page = 10
+        total_pages = (len(products) + items_per_page - 1) // items_per_page
+
+        col1, col2, col2 = st.columns([2, 3, 2])
+        with col2:
+            page = st.number_input("Page", min_value=1, max_value=total_pages, value=1) -1
+
+        start_idx = page * items_per_page
+        end_idx = min(start_idx + items_per_page, len(products))
+
+        st.write(f"Showing {start_idx + 1} - {end_idx} of {len(products)} products")
+
+        for p in products[start_idx:end_idx]:
+            render_product_card(p)
+
+    selected_asin = st.session_state.get("analyzing_asin")
+    if selected_asin:
+        st.divider()
+        st.subheader(f"Competitor analysis for {selected_asin}")
+
+        db = Database()
+        existing_comps = db.search_products({"parent_asin": selected_asin})
+
+        if not existing_comps:
+            with st.spinner("Searching..."):
+                comps = fetch_and_store_competitors(selected_asin, domain, geo)
+
+            st.success(f"Found {len(comps)} competitors!")
+        else:
+            st.info(f"Found {len(existing_comps)} existing competitors in the database.")
+
+        col1, col2 = st.columns([3, 1])
+        with col2:
+            if st.button("Refresh Competitors"):
+                with st.spinner("Refreshing..."):
+                    comps = fetch_and_store_competitors(selected_asin, domain, geo)
+                st.success(f"Found {len(comps)} competitors!")
+
+        with col1:
+            if st.button("Analyze with LLM", type="primary"):
+                with st.spinner("Running LLM..."):
+                    analysis = analyze_competitors(selected_asin)
+                    st.markdown(analysis)
 
 
 if __name__ == "__main__":
